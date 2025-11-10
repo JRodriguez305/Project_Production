57aa6a35574713f6907daab8ab66c4937834a0e0
JRodriguez305 <jrodriguez305@mail.pima.edu>
2025-11-10 16:31:50 -0700

    two scripts + small fixes

---
+++ Assets/Scripts/Door/DoorknobBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorknobBehaviour : MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
