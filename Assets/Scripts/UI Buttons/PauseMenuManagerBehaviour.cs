ec644166e9ba8b7d7fb28caa74f1cd130cc7b0db
JRodriguez305 <jrodriguez305@mail.pima.edu>
2025-11-25 21:50:36 -0700

    Title Change + Pause Script
    - Changed Game Title
    - Added a Pause Menu Script

---
+++ Assets/Scripts/UI Buttons/PauseMenuManagerBehaviour.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PauseMenuManagerBehaviour : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
